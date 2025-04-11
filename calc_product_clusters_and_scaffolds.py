d9ed22f2bc960551aed11fb6244b6dce9dbe8cf2
Allison Walker <allison.s.walker@gmail.com>
2025-04-11 14:16:37 -0500

    product clustering and clustering metrics

---
+++ calc_product_clusters_and_scaffolds.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 11 13:35:43 2025
+
+@author: Allison Walker
+adapted from https://projects.volkamerlab.org/teachopencadd/talktorials/T005_compound_clustering.html
+"""
+
+import pandas as pd
+import numpy
+import matplotlib.pyplot as plt
+from rdkit import Chem
+from rdkit import DataStructs
+from rdkit.ML.Cluster import Butina
+from rdkit.Chem import Draw
+from rdkit.Chem import rdFingerprintGenerator
+from rdkit.Chem import AllChem 
+from rdkit.Chem import DataStructs
+import argparse
+from sklearn import metrics
+import argparse
+
+parser = argparse.ArgumentParser(description='Compares BGC similarity to Tanimoto similarity')
+parser.add_argument('-t', '--tanimoto_file', type=str, default='tanimoto_results/NPAtlas_bm_v1.tsv', help='file containing tanimoto comparison of product structures')
+parser.add_argument('-c', '--butina_threshold', type=float, default=0.2, help='Butina cluster threshold')
+parser.add_argument('-o', '--butina_outfile_name', type=str, default='product_clusters/butina_clusters_pt2.csv', help='file to write butina clusters to')
+parser.add_argument('-o', '--scaffolds_outfile_name', type=str, default='product_scaffolds/bm_scaffolds.csv', help='file to write scaffolds to')
+
+args = parser.parse_args()
+tanimoto_file = args.tanimoto_file
+butina_outfile_name = args.butina_outfile_name
+butina_threshold = args.butina_threshold
+scaffolds_outfile_name = args.scaffolds_outfile_name
+
+def cluster_fingerprints(distance_matrix, num_cmpds, cutoff=0.2):
+    """Cluster fingerprints
+    Parameters:
+        distance matrix
+        cutoff: threshold for the clustering
+    """
+    # Cluster the data with the Butina algorithm:
+    clusters = Butina.ClusterData(distance_matrix, num_cmpds, cutoff, isDistData=True)
+    clusters = sorted(clusters, key=len, reverse=True)
+    return clusters
+
+
+tanimoto_matrix = pd.read_csv(tanimoto_file)
+tanimoto_matrix.set_index('MiBIG_ID', inplace=True)
+tanimoto_distance = 1 - tanimoto_matrix
+
+distance_matrix = tanimoto_distance.values.tolist()
+
+triangular_dist_matrix = []
+for i in range(0, len(distance_matrix)):
+    for j in range(i):
+        triangular_dist_matrix.append(distance_matrix[i][j])
+triangular_dist_matrix = numpy.array(triangular_dist_matrix)
+
+# Run the clustering procedure for the dataset
+clusters = cluster_fingerprints(triangular_dist_matrix, len(tanimoto_distance), cutoff=butina_threshold)
+
+# Give a short report about the numbers of clusters and their sizes
+num_clust_g1 = sum(1 for c in clusters if len(c) == 1)
+num_clust_g5 = sum(1 for c in clusters if len(c) > 5)
+num_clust_g25 = sum(1 for c in clusters if len(c) > 25)
+num_clust_g100 = sum(1 for c in clusters if len(c) > 100)
+
+print("total # clusters: ", len(clusters))
+print("# clusters with only 1 compound: ", num_clust_g1)
+print("# clusters with >5 compounds: ", num_clust_g5)
+print("# clusters with >25 compounds: ", num_clust_g25)
+print("# clusters with >100 compounds: ", num_clust_g100)
+
+
+#write clusters to file
+bgc_list = tanimoto_distance.columns.tolist()
+outfile = open(butina_outfile_name,'w')
+
+i = 0
+bgc_labels = {}
+for c in clusters:
+    outfile.write(str(i))
+    for idx in c:
+        outfile.write(bgc_list[idx] + "," + str(i) + "\n")
+        bgc_labels[bgc_list[idx]] = i
+    outfile.write("\n")
+    i += 1
+outfile.close()
+
+
+label_list = numpy.zeros(len(distance_matrix))
+i = 0
+for bgc in bgc_list:
+    label_list[i] = bgc_labels[bgc]
+    i += 1
+np_dist_matrix =  numpy.zeros((len(distance_matrix),len(distance_matrix)))
+for i in range(0, len(distance_matrix)):
+    for j in range(0, len(distance_matrix)):
+        np_dist_matrix[i][j] = distance_matrix[i][j]
+
+#calculate silhouette score of clustering
+sil = metrics.silhouette_score(np_dist_matrix, numpy.array(label_list), metric='precomputed')
+print("Silhouette score: " + str(sil))
--- calculate_clustering_metrics.py
+++ calculate_clustering_metrics.py
@@ -5,7 +5,11 @@ Created on Fri Apr 11 10:04:28 2025
 @author: Allison Walker
 This script calculates supervised and unsupervised clustering metrics
 """
-
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy
+from sklearn import metrics 
+import numpy as np
 import argparse
 
 parser = argparse.ArgumentParser(description='Compares BGC similarity to Tanimoto similarity')
@@ -13,7 +17,7 @@ parser.add_argument('bgc_cluster_file', type=str, help='file containing BGC clus
 parser.add_argument('bgc_score_file', type=str, help='file containing BGC similarity, must have a format where columns one and two are BGC ids and column 3 is a similarity or distance measurement')
 parser.add_argument('score_type', type=str, help='Score type, options are dist for distance and sim for similarity',choices=["dist","sim"])
 parser.add_argument('-t', '--tanimoto_file', type=str, default='tanimoto_results/NPAtlas_bm_v1.tsv', help='file containing tanimoto comparison of product structures')
-parser.add_argument('-c', '--cluster_file', type=str, default='tanimoto_results/NPAtlas_bm_v1.tsv', help='ground truth cluster file based on structure or user-defined clusters')
+parser.add_argument('-c', '--cluster_file', type=str, default='product_clusters/BGC_butina_cluster_label_0.2.txt', help='ground truth cluster file based on structure or user-defined clusters')
 parser.add_argument('-o', '--outfile_name', type=str, default='tanimoto_bgc_comparison.txt', help='file to write text results to')
 
 args = parser.parse_args()
@@ -24,5 +28,118 @@ tanimoto_file = args.tanimoto_file
 cluster_file = args.cluster_file
 outfile_name = args.outfile_name
 
+outfile = open(outfile_name, 'w')
+#load tanimoto data
+tanimoto_matrix = pd.read_csv(tanimoto_file)
+tanimoto_matrix.set_index('MiBIG_ID', inplace=True)
 
+#load cluster data
+labels = {}
+for line in open(infile_name):
+    split_line = line.split(",")
+    bgc_name = split_line[0]
+    cluster_label = int(split_line[1])
+    labels[bgc_name] = cluster_label
+    
+#load bgc score data    
+scores = {}
+max_score = 0
+for line in open(score_file):
+    split_line = line.split(",")
+    bgc1 = split_line[0]
+    bgc2 = split_line[1]
+    if bgc1 not in labels or bgc2 not in labels:
+        continue
+    score = float(split_line[3])
+    if score > max_score:
+        max_score = score
+    if bgc1 not in scores:
+        scores[bgc1] = {}
+    if bgc2 not in scores:
+        scores[bgc2] = {}
+    #to make score matrix symmetric, take miminimum score if similarity and max if distance
+    if bgc2 in scores[bgc1]:
+        if score_type == "dist" and score < scores[bgc1][bgc2]:
+            scores = scores[bgc1][bgc2]
+        if score_type == "sim" and score > scores[bgc1][bgc2]:
+            scores = scores[bgc1][bgc2]
+    if bgc1 in scores[bgc2]:
+        if score_type == "dist" and score < scores[bgc2][bgc1]:
+            scores = scores[bgc2][bgc1]
+        if score_type == "sim" and score > scores[bgc2][bgc1]:
+            scores = scores[bgc2][bgc1]
+    scores[bgc1][bgc2] = score
+    scores[bgc2][bgc1] = score
+    
+dist_matrix = np.zeros((len(labels),len(labels)))
+tanimoto_matrix_ordered = np.zeros((len(labels),len(labels)))
+label_list = []
+i = 0
+for bgc1 in labels:
+    if bgc1 not in scores:
+        continue
+    label_list.append(labels[bgc1])
+    j =0
+    for bgc2 in labels:
+        tanimoto_matrix_ordered[i][j] = 1- tanimoto_matrix[bgc1[0:bgc1.find(".")]][bgc2[0:bgc2.find(".")]]
+        if bgc2 == bgc1:
+            dist_matrix[i][j] = 0
+            j += 1
+            continue
+        if bgc2 not in scores[bgc1]:
+            dist_matrix[i][j] = 1
+            j += 1
+            continue
+        if score_type == "dist":
+            dist_matrix[i][j] = scores[bgc1][bgc2]
+        else:
+            if max_score > 1:
+                dist_matrix[i][j] = 1- scores[bgc1][bgc2]/max_score
+            else:
+                dist_matrix[i][j] = 1- scores[bgc1][bgc2]       
+        j+=1
+    i += 1
+np.fill_diagonal(dist_matrix, 0)
 
+#Silhouette higher better
+sil = metrics.silhouette_score(dist_matrix, np.array(label_list), metric='precomputed')
+print("silhouette score: " + str(sil))
+outfile.write("silhouette score: " + str(sil) + "\n")
+sil = metrics.silhouette_score(tanimoto_matrix_ordered, np.array(label_list), metric='precomputed')
+print("tanimoto silhouette score: " + str(sil))
+outfile.write("tanimoto silhouette score: " + str(sil) + "\n")
+
+#read in true labels
+true_labels = {}
+for line in open(cluster_file):
+    split_line = line.split(",")
+    true_labels[split_line[0]] = int(split_line[1])
+    
+ordered_true_labels = []
+ordered_pred_labels = []
+for bgc in labels:
+    if bgc not in true_labels:
+        continue
+    ordered_true_labels.append(true_labels[bgc])
+    ordered_pred_labels.append(labels[bgc])
+
+#rand: perfect is 1, in adjusted 0 is random
+#The unadjusted Rand index is proportional to the number of sample pairs whose labels are the same in both labels_pred and labels_true, or are different in both.
+print("rand score: " + str(metrics.rand_score(ordered_true_labels, ordered_pred_labels)))
+outfile.write("rand score: " + str(metrics.rand_score(ordered_true_labels, ordered_pred_labels)) + "\n")
+print("adjusted rand score: " + str(metrics.adjusted_rand_score(ordered_true_labels, ordered_pred_labels)))
+outfile.write("adjusted rand score: " + str(metrics.adjusted_rand_score(ordered_true_labels, ordered_pred_labels)) + "\n")
+#perfect score is 1
+print("mutual information: " + str(metrics.adjusted_mutual_info_score(ordered_true_labels, ordered_pred_labels)))
+outfile.write("mutual information: " + str(metrics.adjusted_mutual_info_score(ordered_true_labels, ordered_pred_labels)) + "\n")
+
+#v score bounded to 1
+print("homogeneity score: " + str(metrics.homogeneity_score(ordered_true_labels, ordered_pred_labels)))
+outfile.write("homogeneity score: " + str(metrics.homogeneity_score(ordered_true_labels, ordered_pred_labels)) + "\n")
+print("completeness score: " + str(metrics.completeness_score(ordered_true_labels, ordered_pred_labels)))
+outfile.write("completeness score: " + str(metrics.completeness_score(ordered_true_labels, ordered_pred_labels)) + "\n")
+print("v score: " + str(metrics.v_measure_score(ordered_true_labels, ordered_pred_labels)))
+outfile.write("v score: " + str(metrics.v_measure_score(ordered_true_labels, ordered_pred_labels)) + "\n")
+#upper bound of 1
+print("fowlkes mallow score: " + str(metrics.fowlkes_mallows_score(ordered_true_labels, ordered_pred_labels)))
+outfile.write("fowlkes mallow score: " + str(metrics.fowlkes_mallows_score(ordered_true_labels, ordered_pred_labels)) + "\n")
